0cfaf129081b9f44a6c91b69bc1fb28a5ca4334b
Peter Bishop <peter@nerdhost.co.uk>
2019-04-24 15:22:20 +0100

    Update tests

---
+++ tests/Feature/RoutingTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class RoutingTest extends TestCase
+{
+    /**
+     * A basic test to ensure routing of core functionality works
+     * as expected.
+     *
+     * @return void
+     */
+    public function testRouting()
+    {
+        // Index/Dashboard
+        $index = $this->get('/');
+        $index->assertStatus(200);
+
+        // Announcements
+        $announcements = $this->get('/announcements');
+        $announcements->assertStatus(200);
+
+        //Service Status
+        $serviceStatus = $this->get('/status');
+        $serviceStatus->assertStatus(200);
+    }
+
+    /**
+     * @depends testRouting
+     */
+    public function testErrors()
+    {
+        // Ensure 404 is displayed when a row does not exist.
+
+        // Row 123 does not exist in the database immediately after install.
+        $announcements = $this->get('/announcements/123');
+        $announcements->assertStatus(404);
+    }
+}
