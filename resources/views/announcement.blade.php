e68933c75e2f0d07c62499bb988342f8cd55612a
Peter Bishop <peter@nerdhost.co.uk>
2019-04-24 14:16:50 +0100

    Minor Updates to templates

--- resources/views/announcement.blade.php
+++ resources/views/announcement.blade.php
@@ -6,10 +6,9 @@
 
 @section('main-content')
         <div class="my-3 p-3 bg-white rounded shadow-sm">
-        <h6 class="border-bottom border-gray pb-2 mb-0">@lang('cosst.announcements')</h6>
+        <h6 class="border-bottom border-gray pb-2 mb-0">{{ $announcement->title }}</h6>
             <div class="media text-muted pt-3">
                 <p class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
-                    <strong class="d-block text-gray-dark">{{ $announcement->title }}</strong>
                     {!! html_entity_decode($announcement->article) !!}
                 </p>
             </div>
--- resources/views/servicestatus.blade.php
+++ resources/views/servicestatus.blade.php
@@ -1,12 +1,13 @@
 @extends('layouts.core')
 
 @section('page-title')
+    @lang('cosst.service_status')
 @endsection
 
 @section('main-content')
     <div class="my-3 p-3 bg-white rounded shadow-sm">
-        <h6 class="border-bottom border-gray pb-2 mb-0">@lang('cosst.announcements')</h6>
-        hfdkjhsdfkjdfhksdfj
+        <h6 class="border-bottom border-gray pb-2 mb-0">@lang('cosst.service_status')</h6>
+            hfdkjhsdfkjdfhksdfj
         <small class="d-block text-right mt-3">
             xbvbvcbvbcvbc
         </small>
