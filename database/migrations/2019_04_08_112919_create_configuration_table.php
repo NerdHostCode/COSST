53bbba88787331ad39069f20fb71b22a52e44d09
Peter Bishop <peter@nerdhost.co.uk>
2019-04-24 14:56:12 +0100

    Update configuration table migration

--- database/migrations/2019_04_08_112919_create_configuration_table.php
+++ database/migrations/2019_04_08_112919_create_configuration_table.php
@@ -15,7 +15,7 @@ class CreateConfigurationTable extends Migration
     public function up()
     {
         Schema::create('configuration', function (Blueprint $table) {
-            $table->string('setting', 250)->unique();
+            $table->string('setting')->unique();
             $table->text('value');
             $table->timestamps();
         });
