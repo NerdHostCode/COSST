ed1e9330ac425d880d32e0c48bc13325940bb7cc
Peter Bishop <peter@nerdhost.co.uk>
2019-05-01 18:21:54 +0100

    Update Tests

--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -31,4 +31,29 @@ class UserTest extends TestCase
 
         $this->assertDatabaseHas('users', $user);
     }
+
+    /**
+     * @depends testUserCreation
+     */
+    public function testUserLogin()
+    {
+        $user = [
+            'email' => 'blackhole@cosst.co.uk',
+            'password' => 'passwordtest'
+        ];
+
+        $response = $this->post('/login', $user);
+
+        $response->assertRedirect('/');
+    }
+
+    /**
+     * @depends testUserLogin
+     */
+    public function testUserLogout()
+    {
+        $response = $this->post('/logout');
+
+        $response->assertRedirect('/');
+    }
 }
