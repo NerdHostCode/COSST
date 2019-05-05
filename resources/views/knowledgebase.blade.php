ab42eabda698d8d1668c0b8b0c1c8fd87f3e50d3
Peter Bishop <9081809+SysAdmin999@users.noreply.github.com>
2019-05-05 20:55:24 +0100

    Add knowledgebase functionality (#12)
    * Add knowledgebase basic functionality
    
    * Add search bar to knowledgebase template

--- resources/views/knowledgebase.blade.php
+++ resources/views/knowledgebase.blade.php
@@ -7,6 +7,20 @@
 @section('main-content')
     <div class="my-3 p-3 bg-white rounded shadow-sm">
         <h6 class="border-bottom border-gray pb-2 mb-0">@lang('cosst.knowledgebase')</h6>
+        <div class="text-center">
+            <form class="form-inline">
+                <div class="input-group mb-3 my-3" style="width: 100%;">
+                    <input type="text" class="form-control" placeholder="@lang('cosst.kbsearchplaceholder')">
+                    <div class="input-group-append">
+                        <button class="btn btn-outline-secondary" type="button">@lang('cosst.search')</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+
+    <div class="my-3 p-3 bg-white rounded shadow-sm">
+        <h6 class="border-bottom border-gray pb-2 mb-0">@lang('cosst.populararticles')</h6>
         @foreach($knowledgebaseArticles as $article)
             <div class="media text-muted pt-3">
                 <p class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
